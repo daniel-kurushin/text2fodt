f786b600a02f6e4723e7c72b05a04e5c3274b384
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-07-26 22:07:36 +0500

    document root for data files

--- text2fodt.py
+++ text2fodt.py
@@ -7,13 +7,15 @@ Created on Wed Jul 10 14:45:19 2019
 """
 
 from textparser import parse
+import os
 
 if __name__ == '__main__':
     import sys
     try:
         fname = sys.argv[1]
+        documentroot = os.path.dirname(fname)
         data = open(fname).read()
-        print(parse(data))
+        print(parse(data, documentroot))
     except IndexError:
         print('usage: %s [input file]' % sys.argv[0])
     except FileNotFoundError:
