1cfdd3320f6409fe1b9a6624cc5e714f3b898635
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-07-26 22:08:29 +0500

    kwargs, exception

--- textelements/paragraph.py
+++ textelements/paragraph.py
@@ -6,9 +6,15 @@ Created on Thu Jul 25 19:46:04 2019
 @author: dan
 """
 
+class ParagraphParseError(Exception):
+    pass
+
 class Paragraph:
     def __init__(self, *args, **kwargs):
-        self.strings = args[0]
+        try:
+            self.strings = kwargs['par']
+        except KeyError:
+            raise ParagraphParseError(str(kwargs))
     
     def __repr__(self):
         return "<p>%s</p>\n" % "\n".join(self.strings)
