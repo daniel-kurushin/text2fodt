7501974271cac523f1371798a3f1f2cba3e0ad35
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-07-26 22:08:57 +0500

    kwargs, exception, enumeration

--- textelements/formula.py
+++ textelements/formula.py
@@ -9,19 +9,24 @@ import re
 
 from textelements.paragraph import Paragraph
 
+FORMULA_N = 1
+
 class FormulaParseError(Exception):
     pass
 
 class Formula(Paragraph):
     def __init__(self, *args, **kwargs):
+        global FORMULA_N
         try:
-            par = args[0]
+            par = kwargs['par']
             self.name, self.content = re.findall(r'\w+?\.\s+(.*?)\s(.*)', par[0])[0]
-        except (IndexError, ValueError):
-            raise FormulaParseError(par)
+            self.no = FORMULA_N
+            FORMULA_N += 1
+        except (IndexError, KeyError, ValueError):
+            raise FormulaParseError(str(kwargs))
     
     def __repr__(self):
-        return '<p>%s</p>' % self.content
+        return '<p>%s<tab/>%s</p>' % (self.content, self.no)
         
 #Form. Ф1 y = f(x) = 3 x^3 + 2 x^2 + b
 
